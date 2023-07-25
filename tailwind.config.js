5f4f9d868459f06fc952f3f56a58584ecb09962a
Tan Lee Ling <tanleeling98@gmail.com>
2023-07-25 17:48:09 +0800

    set_color_theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require("daisyui"), 
-    require('@tailwindcss/typography') 
-  
-],
-  daisyui: {
-    themes: ["cmyk", "dracula"],
-    darkTheme: "dracula",
-  },
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('daisyui'), require('@tailwindcss/typography')],
+	daisyui: {
+		themes: [
+			{
+				cupcake: {
+					...require('daisyui/src/theming/themes')['[data-theme=cupcake]'],
+          
+				}
+			},
+			{
+				dark: {
+					...require('daisyui/src/theming/themes')['[data-theme=dark]'],
+				}
+			}
+		]
+	}
+};
